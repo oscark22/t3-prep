588d4c11ecdb5a112b969922298a47dd9b4d289d
oscarmon12 <98506040+oscarmon12@users.noreply.github.com>
2022-11-11 12:03:44 -0800

    create mutation

---
+++ src/pages/exp.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+
+
+const exp: NextPage = () => {
+    return (<></>);
+}
--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -13,4 +13,19 @@ export const exampleRouter = router({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
+  createDoggo: publicProcedure
+    .input(
+      z.object({
+        color: z.string(),
+        name: z.string()
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.dog.create({
+        data: {
+          color: input.color,
+          name: input.name
+        }
+      })
+    }),
 });
